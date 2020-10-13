a462ba6818e7275e97e9f99a3cfdea68db92e967
addshore <addshore@users.noreply.github.com>
2020-10-13 16:59:44 +0100

    munge, use 0.3.49 as it has the code we need

--- manual/02-munge/execute.sh
+++ manual/02-munge/execute.sh
@@ -2,10 +2,10 @@
 # TODO configurable rdf tools version? (once released..?)
 # TODO configurable dump location & output?
 # TODO ensure that the output directory exists....
-# TODO use the actual release JAR with this code in.... (not the ADDSHORE one)
+wget https://archiva.wikimedia.org/repository/releases/org/wikidata/query/rdf/rdf-spark-tools/0.3.49/rdf-spark-tools-0.3.49-jar-with-dependencies.jar
 gcloud dataproc jobs submit spark --cluster=query-munge-1 \
 --class=org.wikidata.query.rdf.spark.WikidataTurtleDumpConverter \
---jars=gs://wikidata-collab/jar/rdf-spark-tools-0.3.47-SNAPSHOT-jar-with-dependencies-ADDSHORE4.jar \
+--jars=rdf-spark-tools-0.3.49-jar-with-dependencies.jar \
 --properties=spark.master=yarn \
 --properties=spark.executor.memory=25G \
 --properties=spark.executor.cores=8 \
@@ -16,6 +16,6 @@ gcloud dataproc jobs submit spark --cluster=query-munge-1 \
 -- \
 --input-path gs://wikidata-collab/dumps/20200827/wikidata-20200827-all-BETA.ttl.bz2 \
 --num-partitions 512 \
---output-path gs://wikidata-collab/wdqs/20200827/0.3.47/munged/nt \
+--output-path gs://wikidata-collab/wdqs/20200827/0.3.49/munged/nt \
 --output-format nt \
 --skolemize
